38b29a8ede1f181145d9fcca700e41978d9fe1d1
dna241206 <thuyendo176@gmail.com>
2025-12-24 10:44:27 +0700

    Add files via upload

---
+++ Buoi1/Lab1/lab1.js
@@ -0,0 +1,19 @@
+console.log("NamAnh Hehe")
+
+const fullName = ("Do Nam Anh");
+const birthYear = 2006;
+
+const isStudent = true ? "Đúng" : "Sai";
+
+const today = new Date();
+const currentYear = today.getFullYear();
+const calculayeAge = currentYear - birthYear
+
+console.log(`
+    Tên: ${fullName}
+    Tuổi: ${calculayeAge}
+    Sinh viên: ${isStudent}
+    `);
+
+
+
